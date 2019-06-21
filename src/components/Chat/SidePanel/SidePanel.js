0647036d9398533cf11abd1eb7d78ab9b66062a1
AnjuBala <anjubudhwan@gmail.com>
2019-06-21 20:49:06 +0200

    Chat pannel created

---
+++ src/components/Chat/SidePanel/SidePanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UserPanel from "./UserPanel/UserPanel";
+import Channels from "./Channels/Channels";
+
+const SidePanel = () => {
+  return (
+    <>
+      <UserPanel />
+      <Channels />
+    </>
+  );
+};
+
+export default SidePanel;
