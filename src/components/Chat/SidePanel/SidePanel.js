52935b7caecd4fe29b2ee66d1c1cdcc09c0a3106
AnjuBala <anjubudhwan@gmail.com>
2019-06-21 20:59:45 +0200

    UserPanel created

--- src/components/Chat/SidePanel/SidePanel.js
+++ src/components/Chat/SidePanel/SidePanel.js
@@ -1,13 +1,20 @@
 import React from "react";
 import UserPanel from "./UserPanel/UserPanel";
 import Channels from "./Channels/Channels";
+import { Menu } from "semantic-ui-react";
 
 const SidePanel = () => {
   return (
-    <>
+    <Menu
+      size="large"
+      inverted
+      fixed="left"
+      vertical
+      style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
+    >
       <UserPanel />
       <Channels />
-    </>
+    </Menu>
   );
 };
 
--- src/components/Chat/SidePanel/UserPanel/UserPanel.js
+++ src/components/Chat/SidePanel/UserPanel/UserPanel.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Header, Grid, Icon, Dropdown, Image } from "semantic-ui-react";
+import firebase from "../../../../firebase";
 
 const UserPanel = () => {
   return <div>UserPanel</div>;
