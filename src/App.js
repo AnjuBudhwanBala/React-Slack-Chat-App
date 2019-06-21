c9316652dff04ed45220f105e7e5b593e4b8fd0b
AnjuBala <anjubudhwan@gmail.com>
2019-06-21 19:34:09 +0200

    chat component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Chat from "./components/Chat/Chat";
+
 import "./App.css";
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
+        <Route path="/" exact component={Chat} />
       </Switch>
     </div>
   );
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SidePanel from "./SidePanel/SidePanel";
+
+const Chat = () => {
+  return <div>App</div>;
+};
+
+export default Chat;
